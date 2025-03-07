be6e42a6e24f25dd7dd83a8b80f298d1443171cf
bcmenezess <darkdragonbr48@gmail.com>
2025-03-07 02:38:52 -0300

    implemento factory de produtos, admins e usuarios

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'price' => fake()->randomNumber(4,false),
+            'quantity' => fake()->randomNumber(2,false),
+            'description' => fake()->text(),
+            'category' => fake()->word(),
+            'user_id' => User::inRandomOrder()->value('id')
+        ];
+    }
+}
