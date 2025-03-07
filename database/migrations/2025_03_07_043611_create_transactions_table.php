f59bfb230be6d108b8d112942c86e30e0075528e
bcmenezess <darkdragonbr48@gmail.com>
2025-03-07 02:38:10 -0300

    cria model transaction e seus arquivos associados

---
+++ database/migrations/2025_03_07_043611_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->integer('quantity');
+            $table->decimal('total_price', 10, 2);
+            $table->foreignId('buyer_id')->constrained('users')->onDelete('no action');
+            $table->foreignId('product_id')->constrained()->onDelete('no action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
