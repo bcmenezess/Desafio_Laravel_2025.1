6fa137a11cb5665b46964b096dcee3aee0ac1920
bcmenezess <darkdragonbr48@gmail.com>
2025-03-07 06:31:06 -0300

    muda os botoes no welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,8 +30,16 @@
                         </div>
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
-                                @auth
-                                    <a
+                                @if (Auth::guard('web')->check())
+                                        <a
+                                        href="{{ url('/landing-page') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Landing Page
+                                    </a>
+
+                                @elseif (Auth::guard('admin')->check())
+                                        <a
                                         href="{{ url('/dashboard') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
