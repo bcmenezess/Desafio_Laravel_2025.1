a701a6134d10fed2553998475eab52d27ba5948d
bcmenezess <darkdragonbr48@gmail.com>
2025-03-08 05:38:26 -0300

    implementa funcao de view na productController e trata buscas por texto e categoria do usuario

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,15 +5,48 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = usuarioLogado();
+        $query = $request->input('busca');
+        $category = $request->input('category');
+        $categories = Product::select('category')->distinct()->pluck('category');
+        $message = null;
+
+        if($query == ""){
+            $products = Product::where('user_id','!=',$user->id);
+        }
+        else {
+            $products = Product::where('name', 'like', $query)
+                ->orWhere('description', 'like', $query)
+                ->where('user_id','!=',$user->id);
+        }
+
+        if($category != ""){
+            $products = $products->where('category', $category);
+        }
+
+        //dd($products->toSql(), $products->getBindings());
+
+        $products = $products->paginate(5)->appends([
+            'busca' => $query,
+            'category' => $category
+        ]);
+
+
+        if($products->isEmpty()){
+            $message = "Não foi possível encontrar o item buscado";
+        }
+
+        return view('user.landing-page',
+        compact('products','user', 'message', 'categories'));
     }
 
     /**
