63a946be087f0e196190f8dedbd0fb1999cbacaf
bcmenezess <darkdragonbr48@gmail.com>
2025-03-08 05:36:59 -0300

    organiza rotas de acordo com usuario e admin

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,29 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\AuthOrAdminMiddleware;
+use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(AdminMiddleware::class)->name('dashboard');
+Route::prefix('admin')->middleware(AdminMiddleware::class)->group(function () {
+    Route::get('/dashboard',[AdminController::class,'index'])->name('dashboard');
+});
 
-Route::get('/landing-page',[UserController::class,'index'])->middleware('auth')->name('landing-page');
+Route::prefix('user')->middleware(UserMiddleware::class)->group(function (){
+});
 
-Route::middleware('auth')->group(function () {
+Route::get('/landing-page',[ProductController::class,'index'])->name('landing-page');
+//Route::post('/landing-page', [ProductController::class, 'index']);
+
+Route::middleware(AuthOrAdminMiddleware::class)->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
