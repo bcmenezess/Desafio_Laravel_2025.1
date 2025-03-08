465abe1273b40a651d3332c54c59814db5b0da57
bcmenezess <darkdragonbr48@gmail.com>
2025-03-08 05:34:52 -0300

    altera href para rotas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
                             <nav class="-mx-3 flex flex-1 justify-end">
                                 @if (Auth::guard('web')->check())
                                         <a
-                                        href="{{ url('/landing-page') }}"
+                                        href="{{ route('landing-page') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Landing Page
@@ -40,7 +40,7 @@
 
                                 @elseif (Auth::guard('admin')->check())
                                         <a
-                                        href="{{ url('/dashboard') }}"
+                                        href="{{ route('dashboard') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Dashboard
