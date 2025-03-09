8ab8313d0bcd72377ca63cf5bd853e98ed7340c5
bcmenezess <darkdragonbr48@gmail.com>
2025-03-09 02:22:45 -0300

    implementa novos botoes na dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,13 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+    <div class="flex justify-center items-center mt-[100px]">
+        <div class="join join-vertical gap-4">
+            <a class="btn p-8 text-lg join-item" href="">Gerenciamento de usuários</a>
+            <a class="btn p-8 text-lg join-item" href="">Gerenciamento de administradores</a>
+            <a class="btn p-8 text-lg join-item" href="">Gerenciamento de produtos</a>
         </div>
     </div>
 </x-app-layout>
