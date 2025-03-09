3fa21169c2756842b0846bad9716554a63a2d815
bcmenezess <darkdragonbr48@gmail.com>
2025-03-09 04:25:47 -0300

    cria funcao que retorna view da add-user e da funcao post da add-user (que cria e salva usuario no bd)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -17,4 +18,39 @@ class UserController extends Controller
         }
         return view('admin.users-table',compact('users'));
     }
+
+    public function create(){
+        return view('admin.add-user');
+    }
+
+    public function store(StoreUserRequest $request)
+{
+    $validatedData = $request->validated();
+    $logado = usuarioLogado();
+
+    if ($request->hasFile('photo')) {
+        $imagePath = $request->file('photo')->store('profiles', 'public');
+    } else {
+        $imagePath = null;
+    }
+
+    $user = User::create([
+        'name' => $validatedData['name'],
+        'email' => $validatedData['email'],
+        'cpf' => $validatedData['cpf'],
+        'address' => $validatedData['address'],
+        'date_birth' => $validatedData['date_birth'],
+        'telephone' => $validatedData['telephone'],
+        'password' => bcrypt($validatedData['password']),
+        'photo' => $imagePath,
+        'admin_id' => $logado->id,
+        'balance' => 0
+    ]);
+
+    if($user){
+        return redirect()->route('users-table')->with('success', 'Usuário cadastrado com sucesso!');
+    }
+
+    return redirect()->back()->with('error', 'Erro ao cadastrar usuário.');
+}
 }
