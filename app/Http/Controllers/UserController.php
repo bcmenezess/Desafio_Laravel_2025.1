2e388084b6d7c1cabae839ccd78ab69b5b26419a
bcmenezess <darkdragonbr48@gmail.com>
2025-03-09 03:00:02 -0300

    implementa funcao para view da tabela de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(Request $request){
-
+        if(isUser()){
+            $users = usuarioLogado();
+        }
+        else {
+            $users = User::paginate(6);
+        }
+        return view('admin.users-table',compact('users'));
     }
 }
