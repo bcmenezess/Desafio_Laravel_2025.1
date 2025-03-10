9823745a7003201824a4e603a388a62bfb342621
bcmenezess <darkdragonbr48@gmail.com>
2025-03-10 17:45:45 -0300

    adiciona campo para inserir qtd comprada e trata qtd 0 no estoque e usuario admin logado

--- resources/views/user/item-view.blade.php
+++ resources/views/user/item-view.blade.php
@@ -15,7 +15,7 @@
                 </div>
                 <p class="text-lg font-bold text-green-600">R${{$product->price}}</p>
                 <div>
-                    <p class="font-bold">Quantidade:</p>
+                    <p class="font-bold">Em estoque:</p>
                     <p>{{$product->quantity}}</p>
                 </div>
                 <span class="badge">{{$product->category}}</span>
@@ -24,8 +24,20 @@
                     <p>{{$product->description}}</p>
                 </div>
                 <div class="card-actions justify-end">
-                    @if (isUser())
-                        <button class="btn btn-success">Comprar</button>
+                    @if (isAdmin())
+                        <p class="font-bold text-red-800">Sua conta não tem permissão para fazer compras</p>
+                    @elseif (isUser() && $product->quantity > 0)
+                        <form action="{{route('checkout')}}" method="post" class="flex flex-col gap-2">
+                            @csrf
+                            <div class="flex flex-col">
+                                <label for="quantity_input">Insira a quantidade:</label>
+                                <input type="number" name="quantity_input" class="input text-white" value="1" min="1" max="{{$product->quantity}}" required>
+                            </div>
+                            <input type="hidden" name="product" value="{{json_encode($product)}}">
+                            <button class="btn btn-success" type="submit">Comprar</button>
+                        </form>
+                    @else
+                        <p class="font-bold text-red-800">Estoque vazio!</p>
                     @endif
                 </div>
             </div>
