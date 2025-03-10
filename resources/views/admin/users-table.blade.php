d6eb792b060222cd34954d0a09e6314a904b2f4a
bcmenezess <darkdragonbr48@gmail.com>
2025-03-10 02:55:09 -0300

    integra rota de visualizacao de usuario no botao da tabela

--- resources/views/admin/users-table.blade.php
+++ resources/views/admin/users-table.blade.php
@@ -32,7 +32,7 @@
                     <td>{{$user->cpf}}</td>
                     <td>{{$user->telephone}}</td>
                     <td class="flex gap-2">
-                        <button class="btn btn-soft bg-gray-200 text-black">Ver</button>
+                        <a href="users-table/view/{{$user->id}}" class="btn btn-soft bg-gray-200 text-black">Ver</a>
                         <a href="users-table/edit/{{$user->id}}" class="btn btn-warning">Editar</a>
                         <button class="btn btn-error">Deletar</button>
                     </td>
