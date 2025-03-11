1c31fd89205c04770a09ece2fd586a937d9dac67
bcmenezess <darkdragonbr48@gmail.com>
2025-03-11 04:37:05 -0300

    implementa controller e funcionalidades de crud

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use App\Models\Admin;
 use App\Http\Requests\StoreAdminRequest;
 use App\Http\Requests\UpdateAdminRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
@@ -17,51 +19,111 @@ class AdminController extends Controller
         return view('admin.dashboard');
     }
 
+    public function table(){
+        $admins = Admin::paginate(6);
+
+        return view('admin.admins-table',compact('admins'));
+    }
+
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
+    public function create(){
+        return view('admin.add-admin');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreAdminRequest $request)
     {
-        //
+        $validatedData = $request->validated();
+        $logado = usuarioLogado();
+
+        if ($request->hasFile('photo')) {
+            $imagePath = $request->file('photo')->store('profiles', 'public');
+        } else {
+            $imagePath = null;
+        }
+
+        $admin = Admin::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'cpf' => $validatedData['cpf'],
+            'address' => $validatedData['address'],
+            'date_birth' => $validatedData['date_birth'],
+            'telephone' => $validatedData['telephone'],
+            'password' => Hash::make($validatedData['password']),
+            'photo' => $imagePath,
+            'admin_id' => $logado->id,
+        ]);
+
+        if($admin){
+            return redirect()->route('admins-table');
+        }
+
+        return redirect()->back();
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Admin $admin)
-    {
-        //
+    public function editView($id){
+        $admin = Admin::findOrFail($id);
+        return view('admin.edit-admin',compact('admin'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Admin $admin)
-    {
-        //
+    public function edit(UpdateAdminRequest $request, $id){
+        $validatedData = $request->validated();
+        $admin = Admin::find($id);
+
+        if ($request->hasFile('photo')) {
+            if ($admin->photo) {
+                Storage::disk('public')->delete($admin->photo);
+            }
+
+            $imagePath = $request->file('photo')->store('profiles', 'public');
+
+            $admin->photo = $imagePath;
+        }
+
+        if($request->filled('password')){
+            $admin->password = Hash::make($request->password);
+        }
+
+        $admin = $admin->update([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'cpf' => $validatedData['cpf'],
+            'address' => $validatedData['address'],
+            'date_birth' => $validatedData['date_birth'],
+            'telephone' => $validatedData['telephone'],
+        ]);
+
+        if($admin){
+            return redirect()->route('admins-table');
+        }
+
+        return redirect()->back();
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateAdminRequest $request, Admin $admin)
-    {
-        //
+    public function view($id){
+        $admin = Admin::findOrFail($id);
+        return view('admin.view-admin',compact('admin'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Admin $admin)
-    {
-        //
+    public function deleteView($id){
+        $admin = Admin::find($id);
+        return view('admin.delete-admin',compact('admin'));
+    }
+
+    public function delete($id){
+        $admin = Admin::find($id);
+
+        if(isset($admin->photo)){
+            Storage::disk('public')->delete($admin->photo);
+        }
+
+        if(usuarioLogado() == $admin){
+            Auth::logout();
+        }
+
+        $admin->delete();
+
+        return to_route('admins-table');
     }
 }
