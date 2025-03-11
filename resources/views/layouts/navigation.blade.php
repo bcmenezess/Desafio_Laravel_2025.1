df8e9418573c93a39068388e9003a57454e225f4
bcmenezess <darkdragonbr48@gmail.com>
2025-03-11 02:12:47 -0300

    adiciona menu na navbar de acordo com a conta logada

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,21 +1,34 @@
+@php
+    $logado = usuarioLogado();
+@endphp
+
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
-
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('landing-page')" :active="request()->routeIs('landing-page')">
+                        {{ __('Home') }}
                     </x-nav-link>
                 </div>
+
+                @if(isUser())
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('withdraw')" :active="request()->routeIs('withdraw')">
+                            {{ __('Saque') }}
+                        </x-nav-link>
+                    </div>
+
+                @else
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Dashboard') }}
+                        </x-nav-link>
+                    </div>
+                @endif
+
             </div>
 
             <!-- Settings Dropdown -->
