bb6e7b0312621d85332151c0e84f5b13eb4d8b41
bcmenezess <darkdragonbr48@gmail.com>
2025-03-13 02:55:47 -0300

    implementa controller de transacao com index das views e funcoes posts para gerar pdf

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,62 +5,58 @@ namespace App\Http\Controllers;
 use App\Models\Transaction;
 use App\Http\Requests\StoreTransactionRequest;
 use App\Http\Requests\UpdateTransactionRequest;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class TransactionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function purchasesIndex()
     {
-        //
+        $transactions = Transaction::where('buyer_id','=',usuarioLogado()->id)->paginate(3);
+        return view('user.purchase-history',compact('transactions'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function purchasesHistory(){
+        $transactions = Transaction::where('buyer_id','=',usuarioLogado()->id)->get();
+
+        $pdf = Pdf::loadView('user.purchases-pdf', compact('transactions'))->setPaper('a4','landscape');
+        return $pdf->stream('user.purchases-pdf');
+    }
+
+    public function salesIndex()
     {
-        //
+        if(isUser()){
+            $userId = usuarioLogado()->id;
+            $transactions = Transaction::whereHas('product', function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            })->paginate(3);
+        }
+        else{
+            $transactions = Transaction::paginate(3);
+        }
+        return view('user.sell-history',compact('transactions'));
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Show the form for creating a new resource.
      */
-    public function store(StoreTransactionRequest $request)
-    {
-        //
-    }
+    public function salesHistory(){
+        if(isUser()){
+            $userId = usuarioLogado()->id;
+            $transactions = Transaction::whereHas('product', function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            })->get();
+        }
+        else{
+            $transactions = Transaction::all();
+        }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTransactionRequest $request, Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Transaction $transaction)
-    {
-        //
+        $pdf = Pdf::loadView('user.sales-pdf', compact('transactions'))->setPaper('a4','landscape');
+        return $pdf->stream('user.sales-pdf');
     }
 }
